8fb3c5364871dd1894c8b992bd89a520a0baa4cb
Sarvika <Sarvika@student.unsw.edu.au>
2026-02-02 16:33:12 +0400

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Hobbies from "./components/hobbies";
+import Hobbies from "./components/Hobbies";
 
 function App() {
   const [dark, setDark] = useState(false); // default = LIGHT
